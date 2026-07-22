4d315b6da8b1d233322add26ef3ca9a56547c1ac
ishankaradhikari <wrestlenews219@gmail.com>
2026-07-22 13:27:59 +0000

    ComparisionOperations

---
+++ 1.Basic/3_ConversionOperation.js
@@ -0,0 +1,60 @@
+let score = null
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1=> true, 0 => false
+//"" => false, "shankar" => true
+
+let someNumber = 33
+
+let stringNumber =  String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************* Operations ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 3) // 2*2*2
+// console.log(2 % 3) // remainder
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+
+// console.log(str3) // Hello World
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "1")
+// console.log(true)
+
+
+
+let gameCounter = 100
+gameCounter ++
+
+console.log(gameCounter)
