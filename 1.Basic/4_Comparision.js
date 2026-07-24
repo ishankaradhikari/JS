67b86ef31cb84b85bd13a10590332be7a2ecc983
ishankaradhikari <wrestlenews219@gmail.com>
2026-07-24 13:01:54 +0000

    datatype summary

---
+++ 1.Basic/4_Comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" < 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)    // avoid using these comparisions
+// console.log(null ==0)    // avoid using these comparisions
+// console.log(null >=0)    // avoid using these comparisions
+
+// console.log(undefined > 0) // avoid using these comparisions
+
+
+// === 
+
+console.log("2" == 2) //type converted  // true
+console.log("2" === 2) //strict check, checks the data type as well   //false
+
