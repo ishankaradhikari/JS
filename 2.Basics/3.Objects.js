24062e51af149e9929082e7576304f4692b22e14
ishankaradhikari <wrestlenews219@gmail.com>
2026-07-27 04:03:31 +0000

    Objects

---
+++ 2.Basics/3.Objects.js
@@ -0,0 +1,36 @@
+// //singleton -> constructor
+
+// //Object literals
+// let person = {
+//     greet(){
+//         console.log(`Hello, ${user.name} `)
+//     }
+// }
+// user = Object.create(person)
+// user.name = 'Shankar'
+// user.greet()
+
+
+// const mySym = Symbol("Key1")
+
+// const jsUser = {
+//     name: "Shankar",
+//     "full name": "Shankar Adhikari",
+//     [mySym] : "Key1",
+//     age: 20,
+//     location: "Kathmandu",
+//     email: "shankar@gmail.com",
+//     isLoggedIn: true,
+//     lastLoginDays: ['Monday', 'Saturday']
+// }
+
+// console.log(jsUser.name)
+// console.log(jsUser["email"])
+// console.log(jsUser['full name'])
+// console.log(typeof jsUser[mySym])
+
+// jsUser.email = "shankaradhikari@gmail.com"
+// Object.freeze(jsUser)
+// jsUser.name = "Shaknar Adhikari"
+// console.log(jsUser)
+
