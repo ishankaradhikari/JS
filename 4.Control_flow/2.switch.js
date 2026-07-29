9dbc2c6e9418ef8bd27b7f35ffb30d8dc8f2c4af
ishankaradhikari <wrestlenews219@gmail.com>
2026-07-29 03:01:57 +0000

    switch cases

---
+++ 4.Control_flow/2.switch.js
@@ -0,0 +1,36 @@
+//-----------syntax---------
+
+// switch (key){
+//     case value:
+//     break;
+
+// default:
+//     break;
+// }
+
+const month = 'March'
+switch (month) {
+    case 'January':
+        console.log("January");
+        break;
+    case 'February':
+        console.log("February");
+        break;
+    case 'March':
+        console.log("March");
+        break;
+    case 'April':
+        console.log("April");
+        break;
+    case 'June':
+        console.log("June");
+        break;
+    case 'July':
+        console.log("July");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
