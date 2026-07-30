6c9056ac18eed061b5f62171c073b61b1ae0fd34
ishankaradhikari <wrestlenews219@gmail.com>
2026-07-30 08:38:55 +0000

    map() , chaining

---
+++ 5.Iterations/7.seven.js
@@ -0,0 +1,23 @@
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = myNum.map( (num) => {
+//     return num + 10
+// })
+
+
+// Chaining
+
+
+// const newNums = myNum
+//                 .map( ( num ) => num * 10 )
+//                 .map( ( num ) => num + 1 )
+
+
+
+const newNums = myNum
+                .map( ( num ) => num * 10 )
+                .map( ( num ) => num + 1 )
+                .filter( ( num ) => num > 50) // we can add any nymber of methods
+
+
+console.log(newNums);
