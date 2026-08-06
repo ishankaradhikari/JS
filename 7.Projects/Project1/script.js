69b5f8b7314c1d28225c8145bc1f33c85243339a
ishankaradhikari <wrestlenews219@gmail.com>
2026-08-06 11:59:06 +0000

    project 1

--- 7.Projects/Project1/script.js
+++ 7.Projects/Project1/script.js
@@ -1,21 +1,20 @@
-const buttons = document.querySelectorAll('.button')
+const buttons= document.querySelectorAll('.button');
+const body = document.querySelector('body');
 
-const body = document.querySelector('body')
 
-
-buttons.forEach( (button) => {
-    button.addEventListener('click', function(e) {
-        console.log(e);
-        console.log(e.target);
-        if(e.target.id === 'gray'){
-            body.style.backgroundColor = e.target.id;
-        }else if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id;
-        }else if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id;
-        }else if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
+buttons.forEach( function(button){
+    button.addEventListener('click', function(event){
+        if(event.target.id === 'gray'){
+            body.style.backgroundColor = event.target.id;
         }
-        
+        if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }
     })
 })
