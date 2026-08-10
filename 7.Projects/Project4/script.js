1b3847cff2cca1e402eb7e99c03424098b6182c2
ishankaradhikari <wrestlenews219@gmail.com>
2026-08-10 05:47:17 +0000

    loop change

--- 7.Projects/Project4/script.js
+++ 7.Projects/Project4/script.js
@@ -3,7 +3,7 @@ const randomNum = parseInt((Math.random() * 100) + 1)
 const input = document.querySelector('.guessField')
 const submit = document.querySelector('.submitGuess')
 
-// const prev = document.querySelector('.guesses')
+const prev = document.querySelector('.guesses')
 const remain = document.querySelector('.lastResult')
 
 
@@ -13,10 +13,10 @@ submit.addEventListener('click', (e) => {
     e.preventDefault();
     const num = parseInt(document.querySelector('.guessField').value)
 
-    while (num != randomNum){
+    if (num != randomNum){
         document.querySelector('.guesses').innerHTML = `you gussed ${num} but the number was ${randomNum}`
         
-    }{
+    } else{
         document.querySelector('.guesses').innerHTML = `You guessed the correct number ${randomNum}`
     }
 
