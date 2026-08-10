1f329ed1dbf0a2bfdd56a57a3dce2467ce4eed69
ishankaradhikari <wrestlenews219@gmail.com>
2026-08-10 05:42:01 +0000

    progress

---
+++ 7.Projects/Project4/script.js
@@ -0,0 +1,23 @@
+const randomNum = parseInt((Math.random() * 100) + 1)
+
+const input = document.querySelector('.guessField')
+const submit = document.querySelector('.submitGuess')
+
+// const prev = document.querySelector('.guesses')
+const remain = document.querySelector('.lastResult')
+
+
+// console.log(`${prev} ${remain}`)
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const num = parseInt(document.querySelector('.guessField').value)
+
+    while (num != randomNum){
+        document.querySelector('.guesses').innerHTML = `you gussed ${num} but the number was ${randomNum}`
+        
+    }{
+        document.querySelector('.guesses').innerHTML = `You guessed the correct number ${randomNum}`
+    }
+
+})
