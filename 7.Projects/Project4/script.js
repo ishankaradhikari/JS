f9ea990528b94ef553bff4bd26b6a17111512127
ishankaradhikari <wrestlenews219@gmail.com>
2026-08-10 08:37:36 +0000

    Completed Number guessing game

--- 7.Projects/Project4/script.js
+++ 7.Projects/Project4/script.js
@@ -5,19 +5,37 @@ const submit = document.querySelector('.submitGuess')
 
 const prev = document.querySelector('.guesses')
 const remain = document.querySelector('.lastResult')
+const lowHigh = document.querySelector('.lowOrHi')
 
-
+let attempts = 10;
+const arr = new Array()
 // console.log(`${prev} ${remain}`)
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    const num = parseInt(document.querySelector('.guessField').value)
+    const num = parseInt(input.value)
 
-    if (num != randomNum){
-        document.querySelector('.guesses').innerHTML = `you gussed ${num} but the number was ${randomNum}`
+    if (num !== randomNum){
+        arr.push(num);
+        prev.innerHTML = `${arr} `//but the number was ${randomNum}`
         
-    } else{
-        document.querySelector('.guesses').innerHTML = `You guessed the correct number ${randomNum}`
+    }else{
+        prev.innerHTML = `You guessed the correct number ${randomNum}`
+    }
+
+    attempts--
+    remain.innerText = `${attempts}`
+
+    if(num < randomNum){
+        lowHigh.innerHTML = "The number is Higher"
+    }else{
+        lowHigh.innerHTML = "The number is lower"
+    }
+
+    if(attempts === 0){
+        prev.innerHTML = ` The game is over, Number was ${randomNum}`
+        submit.disabled = true;
+        input.disabled = true;
     }
 
 })
